53941c278e1702c25d5b76d1ea1324bb256ddf77
Stephanie Reese <stephanie.anton02@gmail.com>
2014-11-15 09:34:23 -0500

    user model, authentication and home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Gadgetapp::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +54,9 @@ Gadgetapp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  resources :home, :only => [:index]
+
+  root to: "home#index"
+
 end
