d386b6e1b906de97803cd3f38ce0dff90caf9204
Stephanie Reese <stephanie.anton02@gmail.com>
2014-11-15 12:58:14 -0500

    different size images

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,10 +1,8 @@
 class Image < ActiveRecord::Base
-belongs_to :gadget
+	belongs_to :gadget
 
-has_attached_file :photo,
-    :path => ":rails_root/public/images/:id/:filename",
-    :url  => "/images/:id/:filename"
-    
-do_not_validate_attachment_file_type :photo
+	has_attached_file :photo, :styles => {:small => '30x30#', :large => '100x100#'}, :default_url => '/images/:id/photo_:style.png'
+
+  do_not_validate_attachment_file_type :photo
 
 end
